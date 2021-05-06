daac70806d4959c3d77bf19409fdd714932a3b11
IMXNOOBX <69653071+IMXNOOBX@users.noreply.github.com>
2021-05-06 18:42:25 +0200

    Delete AntiTamper directory

--- AntiTamper/IModeHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Confuser.Core;
-
-namespace Confuser.Protections.AntiTamper {
-	internal interface IModeHandler {
-		void HandleInject(AntiTamperProtection parent, ConfuserContext context, ProtectionParameters parameters);
-		void HandleMD(AntiTamperProtection parent, ConfuserContext context, ProtectionParameters parameters);
-	}
-}
